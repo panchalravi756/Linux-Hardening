4956686227e96bc9618f8e2b4f8b9069492ac958
Ravi Kumar Panchal <66041983+panchalravi756@users.noreply.github.com>
2022-03-21 11:58:59 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,19 @@ if [ -f /etc/lsb-release ]; then
     bash ubuntu.sh
 fi
 
-elif [ -f /etc/fedora-release ]; then
+if [ -f /etc/fedora-release ]; then
      bash fedora.sh
 fi
 
-elif [ -f /etc/redhat-release ]; then
+if [ -f /etc/redhat-release ]; then
      bash redhat.sh
 fi
 
-elif [ -f /etc/centos-release ]; then
+if [ -f /etc/centos-release ]; then
      bash centos.sh
 fi
 
-elif [ -f /etc/SuSE-release ] ; then
+if [ -f /etc/SuSE-release ]; then
      bash suse.sh
 fi
 
